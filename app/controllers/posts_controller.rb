a3e8c673ede398625026182b2e559088e66940cc
gratelyas <gratel.yas@gmail.com>
2019-12-13 11:49:05 +0900

    first-commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,21 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order('created_at DESC')
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content, :image)
+  end
+
+  
+end
