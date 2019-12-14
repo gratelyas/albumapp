04b72cdb880412bfb2fade2431235a74456940e6
gratelyas <gratel.yas@gmail.com>
2019-12-14 10:47:01 +0900

    edit-ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
-  post 'posts', to: 'posts#create'
+  # get 'posts', to: 'posts#index'
+  # get 'posts/new', to: 'posts#new'
+  # post 'posts', to: 'posts#create'
+  resources :posts
+  # get 'post', to: 'posts#show'
 end
